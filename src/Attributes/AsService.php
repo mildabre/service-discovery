9805af7d50d15971fdb7aeeca650feca476c6bf1
milos.brecher <milos.brecher@gmail.com>
2026-01-31 17:05:36 +0100

    feat: #[AsService], add optional attribute $name

--- src/Attributes/AsService.php
+++ src/Attributes/AsService.php
@@ -5,7 +5,15 @@ declare(strict_types=1);
 namespace Mildabre\ServiceDiscovery\Attributes;
 
 use Attribute;
+use Nette\InvalidArgumentException;
 
 #[Attribute(Attribute::TARGET_CLASS)]
 class AsService
-{}
+{
+    public function __construct(public readonly ?string $name = null)
+    {
+        if ($this->name === '') {
+            throw new InvalidArgumentException("Empty string is not valid service name.");
+        }
+    }
+}
