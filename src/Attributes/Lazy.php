a6f56a32126682a66315a6e618c903590777e19b
milos.brecher <milos.brecher@gmail.com>
2026-02-17 16:11:28 +0100

    feat: add attribute #[Lazy], to manage lazy service creation

---
+++ src/Attributes/Lazy.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mildabre\ServiceDiscovery\Attributes;
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS)]
+class Lazy
+{
+    public function __construct(public readonly bool|null $enabled = null)
+    {}
+}
